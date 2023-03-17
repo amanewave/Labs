9278f2c140541fb74ac84355fd20edf5a14621f0
amanewave <125118701+amanewave@users.noreply.github.com>
2023-03-17 17:33:16 +1000

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void per(int a, int b, int c,int p);
+
+int main() {
+	per (1, 4, 5);
+	printf("p= %d\n",p)
+	
+	return 0;
+}
+
+void per(int a, int b, int c) {
+	float p = a + b + c;
+	print("Perimetr = %d\n",p);
+}
